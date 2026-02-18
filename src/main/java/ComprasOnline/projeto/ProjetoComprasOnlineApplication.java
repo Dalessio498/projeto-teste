635e15ec4242f5ed06eced6cde9be2555c202e2c
Marco Antonio Dalessio <amtoniomarco91@gmail.com>
2026-02-18 14:17:06 -0300

    Add files via upload

---
+++ src/main/java/ComprasOnline/projeto/ProjetoComprasOnlineApplication.java
@@ -0,0 +1,13 @@
+package ComprasOnline.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoComprasOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoComprasOnlineApplication.class, args);
+	}
+
+}
